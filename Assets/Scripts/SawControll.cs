ec915625f13639824c0fa047d8e1d83acdcd6d61
SanteriMikkola <santerimikkola04@gmail.com>
2020-11-30 11:00:40 +0200

    Level 2 eteenpäin ja lisäsin sahan.

---
+++ Assets/Scripts/SawControll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawControll : MonoBehaviour
+{
+    public float RotSpeed;
+    public Transform Pos1, pos2;
+
+    Vector2 nextPos;
+    public Transform startPos;
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+    void Update()
+    {
+        if (transform.position == Pos1.position)
+        {
+            nextPos = pos2.position;
+            transform.Rotate(0, 0, RotSpeed);
+        }
+
+        if (transform.position == pos2.position)
+        {
+            nextPos = Pos1.position;
+            transform.Rotate(0, 0, RotSpeed);
+        }
+    }
+}
