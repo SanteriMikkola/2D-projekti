ae77394df988f8f0bbcc52498a94f0a25b84f0f9
SanteriMikkola <santerimikkola04@gmail.com>
2020-11-30 16:16:18 +0200

    Sahan väsäystä.

--- Assets/Scripts/SawControll.cs
+++ Assets/Scripts/SawControll.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SawControll : MonoBehaviour
 {
-    public float RotSpeed;
+    public float RotationSpeed;
     public Transform Pos1, pos2;
-
+    public GameObject Saw;
+    public float speed;
     Vector2 nextPos;
     public Transform startPos;
+
+    public bool rotationControll;
+    public float RotZ;
     void Start()
     {
         nextPos = startPos.position;
@@ -17,14 +21,26 @@ public class SawControll : MonoBehaviour
     {
         if (transform.position == Pos1.position)
         {
+            Saw.transform.Rotate(0, 0, RotationSpeed);
             nextPos = pos2.position;
-            transform.Rotate(0, 0, RotSpeed);
+            RotZ += Time.deltaTime * RotationSpeed;
         }
 
         if (transform.position == pos2.position)
         {
             nextPos = Pos1.position;
-            transform.Rotate(0, 0, RotSpeed);
+            Saw.transform.Rotate(0, 0, RotationSpeed);
+            RotZ += Time.deltaTime * RotationSpeed;
         }
+        transform.position = Vector2.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        if (rotationControll == false)
+        {
+            RotZ += Time.deltaTime * RotationSpeed;
+        }
+        else
+        {
+            RotZ += Time.deltaTime * RotationSpeed;
+        }
+        transform.rotation = Quaternion.Euler(0, 0, RotZ);
     }
 }
